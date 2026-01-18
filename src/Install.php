f3c5f57851da6b0183d2b991401b9252324568d3
aihe <torghay@yeah.net>
2026-01-18 17:40:29 +0800

    🔧 chore(Install): add recursive flag to copy_dir function
    - 为 copy_dir 函数添加第三个参数 true，确保目录递归复制
    - 修复可能存在的目录复制不完整问题

--- src/Install.php
+++ src/Install.php
@@ -50,7 +50,7 @@ class Install
                 }
             }
             // symlink(__DIR__ . "/$source", base_path()."/$dest");
-            copy_dir(__DIR__ . "/$source", base_path() . "/$dest");
+            copy_dir(__DIR__ . "/$source", base_path() . "/$dest", true);
             echo "Create $dest
 ";
         }
